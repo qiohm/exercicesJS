bd626b82cd0d97ef10c8693381e36fd069244ee2
Gé Pé <aiohm@yahoo.fr>
2020-09-22 15:06:19 +0200

    2eme exercice

---
+++ src/ex02.js
@@ -0,0 +1,11 @@
+/*
+for (let countdown = 100; countdown >= 0; countdown -= 2) {
+    console.log(countdown)
+}
+*/
+
+let cDown = 100
+while (cDown >= 0) {
+    console.log(cDown)
+    cDown -= 2
+}
