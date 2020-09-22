83917ed239feb441b87134fc5f697b9aedc0d929
Gé Pé <aiohm@yahoo.fr>
2020-09-23 00:20:02 +0200

    add ex7

--- src/ex07.js
+++ src/ex07.js
@@ -9,3 +9,54 @@ let nb2 = calc('-', 6, 4) // nb2 === 2
 let nb3 = calc('*', 4, 5) // nb3 === 20
 let nb4 = calc('/', 10, 2) // nb4 == 5
 */
+
+const add = (a, b) => {
+    return a + b
+}
+
+const sub = (a, b) => {
+    return a - b
+}
+
+const mul = (a, b) => {
+    return a * b
+}
+
+const div = (a, b) => {
+    return a / b
+}
+
+/* const calc = (op, a, b) => {
+    if (op==='+') {
+        add(a,b)
+    } else if 
+        (op==='-') {
+            sub(a,b)
+         } else if 
+            (op==='*') {
+                mul(a,b)
+            } else if 
+                (op==='\') {
+                    div(a,b)
+                }
+                else {
+                    console.log('42 is the answer to life, the universe and everything that matters')
+                }
+            }
+    
+    */
+
+const calc = (op, a, b) => {
+    switch (op) {
+        case '+':
+            return add(a, b)
+        case '-':
+            return sub(a, b)
+        case '*':
+            return mul(a, b)
+        case '/':
+            return div(a, b)
+        default:
+            console.log('Nop: unknown operator')
+    }
+}
