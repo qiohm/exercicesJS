5ef78207ae4ca084afa1a03ee433fb7c6beb89c3
Gé Pé <aiohm@yahoo.fr>
2020-09-23 01:38:55 +0200

    add ex08

--- src/ex08.js
+++ src/ex08.js
@@ -1,4 +1,17 @@
 'use strict'
 /*
-
+ex08.js:
+Ecrivez une fonction isPalindrome qui prend une string en paramètre. 
+Cette fonction retourne true si le paramètre est un palindrome sinon retourne false.
+isPalindrome('tenet') // retruns true
+isPalindrome('Alyra') // returns false
 */
+
+const isPalindrome = (str) => {
+    str = str.toLowerCase()
+    return str.split('').reverse().join('') == str
+}
+console.log(isPalindrome('Tenet'))
+console.log(isPalindrome('Alyra'))
+console.log(isPalindrome('xanax'))
+console.log(isPalindrome('rêver'))
