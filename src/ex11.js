30705148f4908a0f48e9b97ea0862a67aafdf682
Gé Pé <aiohm@yahoo.fr>
2020-09-23 14:10:24 +0200

    useless but sheer commit

--- src/ex11.js
+++ src/ex11.js
@@ -1,4 +1,7 @@
 'use strict'
 /*
-
+ex11.js:
+Ecrivez une fonction makeUnique qui prend comme paramètre une liste et enlève tous les doublons, 
+et retourne cette cette liste classée par ordre croissant.
+makeUnique([1, 2, 1, 3, 2, 4, 5, 7, 5, 1]) //returns [1, 2, 3, 4, 5, 7]
 */
